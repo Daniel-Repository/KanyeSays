da5414fceae22da18ba526aab68ff3864c462fc1
Daniel Carroll <danielcarrollbusiness@gmail.com>
2022-06-03 16:18:43 +0100

    Multiline support
    Additional room in quote container so that if the quote expands beyond 1 line, the container won't expand

--- app.js
+++ app.js
@@ -2,16 +2,6 @@ const kanye_api_url = 'https://api.kanye.rest/';
 
 let kanyeQuote;
 
-//Fetches the quote from the KanyeAPI. Inserts appropriate text into element.
-// async function getKanyeQuote() {
-//     document.getElementById("quote").innerHTML = "Loading...";
-//     const response = await fetch(kanye_api_url);
-//     const data = await response.json();
-//     console.log(data.quote);
-//     kanyeQuote = data.quote;
-//     document.getElementById("quote").innerHTML = "\"" + data.quote + "\"";
-// }
-
 const getKanyeQuote = async () => {
     const res = await fetch(kanye_api_url);
     const data = await res.json();
